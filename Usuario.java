5161b86568e94f2f3a15938c0bc7cff865627d92
Vitor Sacchi Sanches <119360596+vitorsacchi@users.noreply.github.com>
2025-05-05 11:13:13 -0300

    Estudos2.0
    commit2 atualização de usuario, smarttv, e myclass

---
+++ Usuario.java
@@ -0,0 +1,45 @@
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private int idade;
+
+    public Usuario(String nome, String email, String senha, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
